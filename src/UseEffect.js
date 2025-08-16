25f7515f9ed8c7106f10b59511095a04c8355f78
itsluckysharma01 <panditluckysharma42646@gmail.com>
2025-08-16 18:18:08 +0530

    Add useEffect

--- src/UseEffect.js
+++ src/UseEffect.js
@@ -2,20 +2,33 @@ import React, { useState, useEffect } from 'react'
 
 const UseEffect = () =>{
 
-  const [clicked, setClicked] = useState();
+const [clicked, setClicked] = useState();
+const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    console.log(clicked);
-  }, [clicked]);
+
+  useEffect(()=>{
+    // alert('we are in new state')
+    console.log('we are in new state')
+  },[clicked,count]);
+
 
   return (
     <div>
       <button onClick={() => setClicked('Hello')}>Hello</button>
       <br></br>
+      <hr></hr>
       <button onClick={() => setClicked('Lucky Sharma')}>Lucky Sharma</button><br></br>
+      <hr></hr>
       <button onClick={() => setClicked('Bharmouri')}>Bharmouri</button>
-
+      <hr></hr>
+      
       <h1>{clicked}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      
+
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+      <h1>{count}</h1>
+      
     </div>
   );
 }
