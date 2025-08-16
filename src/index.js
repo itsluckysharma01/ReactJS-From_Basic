1f4f3c0858835155dc21d0277b029e72a61a2d53
itsluckysharma01 <panditluckysharma42646@gmail.com>
2025-08-16 20:48:02 +0530

    Add FectchuseEffect

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import Data from './Data.js'
-// import Form from './From.js';
-import UseEffect from './UseEffect.js';
+import Fetch from './Fetch';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <UseEffect />
-
+    <Fetch/>
 
   </React.StrictMode>
 );
