515c9635a27331670d9889e734309a34b973cc39
itsluckysharma01 <panditluckysharma42646@gmail.com>
2025-09-05 20:57:58 +0530

    useLayoutEffect Hook

--- src/Uselayoutexample.js
+++ src/Uselayoutexample.js
@@ -1,3 +1,23 @@
 // useEffect
 // uselayoutEffect
-import React, { useState, useEffect, useLayoutEffect } from 'react';
+import React, { useState, useEffect, useLayoutEffect } from "react";
+
+const Uselayoutexample = () => {
+  useEffect(() => {
+    console.log("useEffect Part");
+  }, []);
+
+  useLayoutEffect(() => {
+    //disply first then useEffect
+    console.log("useLayoutEffect Part");
+  }, []);
+
+  return (
+    <div>
+      <h1>useEffect vs useLayoutEffect</h1>
+      <p>Check the console to see the order of execution.</p>
+    </div>
+  );
+};
+
+export default Uselayoutexample;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Parent from './Parent';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Uselayoutexample from "./Uselayoutexample";
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-
-  <Parent />
-    
+    <Uselayoutexample />
   </React.StrictMode>
 );
